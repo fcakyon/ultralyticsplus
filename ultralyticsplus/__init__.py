f3f791601aa26f41b44cb0cc1b8fa3cb9d23f7ce
fcakyon <fcakyon@gmail.com>
2023-01-21 23:10:05 +0300

    update version

--- ultralyticsplus/__init__.py
+++ ultralyticsplus/__init__.py
@@ -1,4 +1,4 @@
 from .hf_utils import download_from_hub, push_to_hfhub
 from .ultralytics_utils import YOLO, postprocess_classify_output, render_result
 
-__version__ = "0.0.14"
+__version__ = "0.0.15"
